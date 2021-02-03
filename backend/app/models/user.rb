ae8a1e2c12acbd0d69fd7ed7a8f5d2f10268d94d
Smys <krzysztofmichalski987@gmail.com>
2021-02-03 11:50:05 +0100

    phone number validation

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
                       :uniqueness => true
     validates :nickname,  :presence => true,
                       :uniqueness => true
-    # validates :phone, :numericality => true,
-    #                   :presence => false,
-    #                   :length => {:minumum => 10, :maximum => 15}
+    validates :phone, :numericality => true,
+                      :length => {:minumum => 10, :maximum => 15},
+                      :allow_blank => true
     has_secure_password
 end
