162be0e672c214f81b2b0473c095b98be214984a
Smys <krzysztofmichalski987@gmail.com>
2021-02-03 11:54:02 +0100

    add email validation

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
     validates :email, :presence => true,
-                      :uniqueness => true
+                      :uniqueness => true,
+                      :format => { with: URI::MailTo::EMAIL_REGEXP }
     validates :nickname,  :presence => true,
                       :uniqueness => true
     validates :phone, :numericality => true,
-                      :length => {:minumum => 10, :maximum => 15},
+                      :length => { :minumum => 10, :maximum => 15 },
                       :allow_blank => true
     has_secure_password
 end
