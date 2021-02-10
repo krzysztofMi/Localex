690ed0047367544d5aa06c5c8a4a2c096042eccc
Smys <krzysztofmichalski987@gmail.com>
2021-02-10 11:51:57 +0100

    remove old backend dir

--- backend/app/models/role.rb
+++
@@ -1,9 +0,0 @@
-class Role < ApplicationRecord
-    has_many :users
-    self.inheritance_column = nil
-    enum type: {
-        admin: "ADMIN",
-        user: "USER",
-        moderator: "MODERATOR"
-    }
-end
