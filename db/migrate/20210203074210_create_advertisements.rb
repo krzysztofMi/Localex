982fd043ce480a67a67338707660cb49cfe0aca6
Smys <krzysztofmichalski987@gmail.com>
2021-02-10 11:42:04 +0100

    remove frontend part of application from gitdir

---
+++ db/migrate/20210203074210_create_advertisements.rb
@@ -0,0 +1,19 @@
+class CreateAdvertisements < ActiveRecord::Migration[6.1]
+  def change
+
+    execute <<-DDL
+      CREATE Type advertisements_type AS ENUM (
+        'free', 'exchange', 'pay'
+      );
+    DDL
+
+    create_table :advertisements do |t|
+      t.string :name, null: false
+      t.column :type, :advertisements_type, null: false
+      t.text :description, default: "" 
+      t.string :place
+      
+      t.timestamps
+    end
+  end
+end
