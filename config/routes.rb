21db4a5b5bbab169387df556a0514dd485480233
Smys <krzysztofmichalski987@gmail.com>
2021-02-10 11:45:31 +0100

    remove unnecessary route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :users, only: [:create]
-      get "/users", to: "users#show" 
       post "/login", to: "users#login"
       get "/auto_login", to: "users#auto_login"
       resources :advertisements 
